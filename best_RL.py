09a9b26f4092adcc90007627f9aabc15c3c379ce
Catalina Tan <zcemcat@ucl.ac.uk>
2025-03-12 21:08:08 +0000

    updated file path

--- best_RL.py
+++ best_RL.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import gym
 import torch
-from new_file_copy import Net
+from net_global import Net
 import torch.nn.functional as F
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import DummyVecEnv
